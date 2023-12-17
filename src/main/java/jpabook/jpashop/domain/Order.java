61dafbf21b3066920c5c0a17f7ad93e35b299152
leeju0 <92801522+leeju0@users.noreply.github.com>
2023-12-17 16:35:48 +0900

    entity 만드는중

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member; //주문 회원
+
+    private List<OrderItem> orderItems = new ArrayList<>();
+    private Delivery delivery;
+    private LocalDateTime orderDate;
+    private OrderStatus status;
+
+}
